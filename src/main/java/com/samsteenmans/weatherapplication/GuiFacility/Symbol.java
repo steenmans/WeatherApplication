9409825dd277d1da6dcf6557d000bb1a724b890e
samst <samsteenmans@hotmail.com>
2022-03-22 19:40:19 +0100

    GUI: Current word vernieuwd bezig aan de dagen
    Symbol klasse aangepast,url's klopt niet

--- src/main/java/com/samsteenmans/weatherapplication/GuiFacility/Symbol.java
+++ src/main/java/com/samsteenmans/weatherapplication/GuiFacility/Symbol.java
@@ -2,26 +2,26 @@ package com.samsteenmans.weatherapplication.GuiFacility;
 
 public class Symbol {
 
-    public String getWeatherSymbolURL(int weatherCondition) {
+    public String getWeatherSymbolURL(long weatherCondition) {
         String weatherSymbolURL;
 
         // switch statement to check size
-        switch (weatherCondition) {
+        switch ((int) weatherCondition) {
             case 1000: //Sunny
-                weatherSymbolURL = "@weather/WhiteIcons/sun.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/sun4.png";
                 break;
 
             case 1003: // Partly Cloudy
-                weatherSymbolURL = "@weather/WhiteIcons/clouds.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/clouds.png";
                 break;
 
             case 1006: // Cloudy
             case 1009: // Overcast
-                weatherSymbolURL = "@weather/WhiteIcons/cloudy.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/cloudy.png";
                 break;
 
             case 1030: // Partly Cloudy
-                weatherSymbolURL = "@weather/WhiteIcons/partlycloudyday.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/partlycloudyday.png";
                 break;
 
             case 1063: // Patchy Rain Possible
@@ -30,22 +30,22 @@ public class Symbol {
             case 1114: // Blowing Snow
             case 1210: // Patchy Light Snow
             case 1213: // Light Snow
-                weatherSymbolURL = "@weather/WhiteIcons/littlesnow.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/littlesnow.png";
                 break;
 
             case 1087: // Thundery Outbreaks possible
-                weatherSymbolURL = "@weather/WhiteIcons/chanceofstorm.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/chanceofstorm.png";
                 break;
 
             case 1117: // Blizzard
             case 1279: // Patchy Light Snow With Thunder
             case 1282: // Moderate Or Heavy Snow With Thunder
-                weatherSymbolURL = "@weather/WhiteIcons/snowstorm.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/snowstorm.png";
                 break;
 
             case 1135: // Fog
             case 1147: // Freezing fog
-                weatherSymbolURL = "@weather/WhiteIcons/fogday.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/fogday.png";
                 break;
 
             case 1150: // Patchy Little Rain
@@ -53,7 +53,7 @@ public class Symbol {
             case 1180: // Patchy light Rain
             case 1183: // Light Rain
             case 1240: // Light Rain Shower
-                weatherSymbolURL = "@weather/WhiteIcons/littlerain.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/littlerain.png";
                 break;
 
             case 1168: // Freezing Drizzle
@@ -62,11 +62,11 @@ public class Symbol {
             case 1237: // Ice Pellets
             case 1261: // Light Showers Of Ice Pellets
             case 1264: // Moderate Or Heavy Showers Of Ice Pellets
-                weatherSymbolURL = "@weather/WhiteIcons/icy.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/icy.png";
                 break;
 
             case 1189: // Moderate Rain
-                weatherSymbolURL = "@weather/WhiteIcons/rainyweather.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/rainyweather.png";
                 break;
 
             case 1192: // Heavy Rain At Times
@@ -74,30 +74,30 @@ public class Symbol {
             case 1201: // Moderate Or Heavy Freezing Rain
             case 1243: // Moderate Or Heavy Rainshower
             case 1246: // Torrential Rain Shower
-                weatherSymbolURL = "@weather/WhiteIcons/rain.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/rain.png";
                 break;
 
             case 1216: // Patchy Moderate Snow
             case 1219: // Moderate Snow
             case 1222: // Patchy Heavy Snow
             case 1225: // Heavy Snow
-                weatherSymbolURL = "@weather/WhiteIcons/snow.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/snow.png";
                 break;
 
             case 1249: // Light Sleet Shower
             case 1252: // Moderate or Heavy Sleet Showers
             case 1255: // Light Snow Showers
             case 1258: // Moderate Or Heavy Snow Showers
-                weatherSymbolURL = "@weather/WhiteIcons/sleet.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/sleet.png";
                 break;
 
             case 1273: // Patchy Light Rain With Thunder
             case 1276: // Moderate Or Heavy Rain With Thunder
-                weatherSymbolURL = "@weather/WhiteIcons/storm.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/storm.png";
                 break;
 
             default:
-                weatherSymbolURL = "@weather/WhiteIcons/moon.png";
+                weatherSymbolURL = "src/main/resources/com/samsteenmans/weatherapplication/weather/WhiteIcons/moon.png";
                 break;
         }
         return weatherSymbolURL;
--- src/main/java/com/samsteenmans/weatherapplication/MainScreenController.java
+++ src/main/java/com/samsteenmans/weatherapplication/MainScreenController.java
@@ -1,35 +1,38 @@
 package com.samsteenmans.weatherapplication;
 
+import com.samsteenmans.weatherapplication.GuiFacility.Symbol;
 import com.samsteenmans.weatherapplication.data.CompleteWeatherData;
 import com.samsteenmans.weatherapplication.data.Data;
 import com.samsteenmans.weatherapplication.data.autocomplete.AutoCompleteLocations;
+import com.samsteenmans.weatherapplication.data.current.Current;
+import com.samsteenmans.weatherapplication.data.forecast.ForecastDay;
 import com.samsteenmans.weatherapplication.weatherData.WeatherData;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.VBox;
 import org.controlsfx.control.textfield.TextFields;
 
+import java.io.File;
 import java.util.List;
 
 public class MainScreenController {
     /* *** --------- ***
     // *** Variables ***
     // *** --------- ***/
+    @FXML // fx:id="textFieldSearch"
+    private TextField textFieldSearch; // Value injected by FXMLLoader
+
     @FXML // fx:id="areaChart"
     private AreaChart<?, ?> areaChart; // Value injected by FXMLLoader
 
     @FXML // fx:id="currentConditionImageViewIcon"
     private ImageView currentConditionImageViewIcon; // Value injected by FXMLLoader
-
-    @FXML // fx:id="currentConditionLabelPressure"
-    private Label currentConditionLabelPressure; // Value injected by FXMLLoader
-
-    @FXML // fx:id="currentConditionLabelRain"
-    private Label currentConditionLabelRain; // Value injected by FXMLLoader
 
     @FXML // fx:id="currentConditionLabelState"
     private Label currentConditionLabelState; // Value injected by FXMLLoader
@@ -37,98 +40,104 @@ public class MainScreenController {
     @FXML // fx:id="currentConditionLabelTemperature"
     private Label currentConditionLabelTemperature; // Value injected by FXMLLoader
 
+    @FXML // fx:id="currentConditionLabelPressure"
+    private Label currentConditionLabelPressure; // Value injected by FXMLLoader
+
+    @FXML // fx:id="currentConditionLabelRain"
+    private Label currentConditionLabelRain; // Value injected by FXMLLoader
+
     @FXML // fx:id="currentConditionLabelWind"
     private Label currentConditionLabelWind; // Value injected by FXMLLoader
 
-    @FXML // fx:id="fifthBlockImageViewIcon"
-    private ImageView fifthBlockImageViewIcon; // Value injected by FXMLLoader
+    @FXML // fx:id="blockVBox1"
+    private VBox blockVBox1; // Value injected by FXMLLoader
 
-    @FXML // fx:id="fifthBlockLabelDay"
-    private Label fifthBlockLabelDay; // Value injected by FXMLLoader
+    @FXML // fx:id="imageViewIcon1"
+    private ImageView imageViewIcon1; // Value injected by FXMLLoader
 
-    @FXML // fx:id="fifthBlockLabelMaxTemperature"
-    private Label fifthBlockLabelMaxTemperature; // Value injected by FXMLLoader
+    @FXML // fx:id="labelState1"
+    private Label labelState1; // Value injected by FXMLLoader
 
-    @FXML // fx:id="fifthBlockLabelMinTemperature"
-    private Label fifthBlockLabelMinTemperature; // Value injected by FXMLLoader
+    @FXML // fx:id="labelMaxTemperature1"
+    private Label labelMaxTemperature1; // Value injected by FXMLLoader
 
-    @FXML // fx:id="fifthBlockLabelState"
-    private Label fifthBlockLabelState; // Value injected by FXMLLoader
-
-    @FXML // fx:id="firstBlockImageViewIcon"
-    private ImageView firstBlockImageViewIcon; // Value injected by FXMLLoader
-
-    @FXML // fx:id="firstBlockLabelMaxTemperature"
-    private Label firstBlockLabelMaxTemperature; // Value injected by FXMLLoader
-
-    @FXML // fx:id="firstBlockLabelMinTemperature"
-    private Label firstBlockLabelMinTemperature; // Value injected by FXMLLoader
-
-    @FXML // fx:id="firstBlockLabelState"
-    private Label firstBlockLabelState; // Value injected by FXMLLoader
-
-    @FXML // fx:id="firstBlockVBox"
-    private VBox firstBlockVBox; // Value injected by FXMLLoader
-
-    @FXML // fx:id="fourthBlockImageViewIcon"
-    private ImageView fourthBlockImageViewIcon; // Value injected by FXMLLoader
-
-    @FXML // fx:id="fourthBlockLabelDay"
-    private Label fourthBlockLabelDay; // Value injected by FXMLLoader
-
-    @FXML // fx:id="fourthBlockLabelState"
-    private Label fourthBlockLabelState; // Value injected by FXMLLoader
-
-    @FXML // fx:id="fourthBlockMaxTemperature"
-    private Label fourthBlockMaxTemperature; // Value injected by FXMLLoader
-
-    @FXML // fx:id="fourthBlockMinTemperature"
-    private Label fourthBlockMinTemperature; // Value injected by FXMLLoader
-
-    @FXML // fx:id="secondBlockImageViewIcon"
-    private ImageView secondBlockImageViewIcon; // Value injected by FXMLLoader
-
-    @FXML // fx:id="secondBlockLabelDay"
-    private Label secondBlockLabelDay; // Value injected by FXMLLoader
-
-    @FXML // fx:id="secondBlockLabelMaxTemperature"
-    private Label secondBlockLabelMaxTemperature; // Value injected by FXMLLoader
-
-    @FXML // fx:id="secondBlockLabelMinTemperature"
-    private Label secondBlockLabelMinTemperature; // Value injected by FXMLLoader
-
-    @FXML // fx:id="secondBlockLabelState"
-    private Label secondBlockLabelState; // Value injected by FXMLLoader
+    @FXML // fx:id="labelMinTemperature1"
+    private Label labelMinTemperature1; // Value injected by FXMLLoader
 
     @FXML // fx:id="secondBlockVBox"
     private VBox secondBlockVBox; // Value injected by FXMLLoader
 
-    @FXML // fx:id="textFieldSearch"
-    private TextField textFieldSearch; // Value injected by FXMLLoader
+    @FXML // fx:id="secondBlockLabelDay"
+    private Label secondBlockLabelDay; // Value injected by FXMLLoader
 
-    @FXML // fx:id="thirdBlockImageViewIcon"
-    private ImageView thirdBlockImageViewIcon; // Value injected by FXMLLoader
+    @FXML // fx:id="imageViewIcon2"
+    private ImageView imageViewIcon2; // Value injected by FXMLLoader
 
-    @FXML // fx:id="thirdBlockLabelDay"
-    private Label thirdBlockLabelDay; // Value injected by FXMLLoader
+    @FXML // fx:id="labelState2"
+    private Label labelState2; // Value injected by FXMLLoader
 
-    @FXML // fx:id="thirdBlockLabelMaxTemperature"
-    private Label thirdBlockLabelMaxTemperature; // Value injected by FXMLLoader
+    @FXML // fx:id="labelMaxTemperature2"
+    private Label labelMaxTemperature2; // Value injected by FXMLLoader
 
-    @FXML // fx:id="thirdBlockLabelMinTemperature"
-    private Label thirdBlockLabelMinTemperature; // Value injected by FXMLLoader
-
-    @FXML // fx:id="thirdBlockLabelState"
-    private Label thirdBlockLabelState; // Value injected by FXMLLoader
+    @FXML // fx:id="labelMinTemperature2"
+    private Label labelMinTemperature2; // Value injected by FXMLLoader
 
     @FXML // fx:id="thirdBlockVBox"
     private VBox thirdBlockVBox; // Value injected by FXMLLoader
 
-    // Initialize
+    @FXML // fx:id="thirdBlockLabelDay"
+    private Label thirdBlockLabelDay; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageViewIcon3"
+    private ImageView imageViewIcon3; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelState3"
+    private Label labelState3; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelMaxTemperature3"
+    private Label labelMaxTemperature3; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelMinTemperature3"
+    private Label labelMinTemperature3; // Value injected by FXMLLoader
+
+    @FXML // fx:id="blockLabelDay4"
+    private Label blockLabelDay4; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageViewIcon4"
+    private ImageView imageViewIcon4; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelState4"
+    private Label labelState4; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelMaxTemperature4"
+    private Label labelMaxTemperature4; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelMinTemperature4"
+    private Label labelMinTemperature4; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelDay5"
+    private Label labelDay5; // Value injected by FXMLLoader
+
+    @FXML // fx:id="imageViewIcon5"
+    private ImageView imageViewIcon5; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelState5"
+    private Label labelState5; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelMaxTemperature5"
+    private Label labelMaxTemperature5; // Value injected by FXMLLoader
+
+    @FXML // fx:id="labelMinTemperature5"
+    private Label labelMinTemperature5; // Value injected by FXMLLoader
+
+
+
+    /* *** --------- ***
+    // *** Initialize ***
+    // *** --------- ***/
     public void initialize(){
         WeatherData weatherData = new WeatherData();
         Data data = new Data();
-        weatherData.setMeasurements(data.getCompleteWeatherData("duffel",5));
 
 
         // Add a listener to the Search textField for giving autocomplete
@@ -150,18 +159,48 @@ public class MainScreenController {
         // Listener if entered is pressed in the textField
         textFieldSearch.setOnKeyPressed( event -> {
             if( event.getCode() == KeyCode.ENTER ) {
-                weatherData.setMeasurements(data.getCompleteWeatherData(textFieldSearch.getText(),5));
+                setFxmlData(data.getCompleteWeatherData(textFieldSearch.getText(),5));
             }
         } );
     }
 
-    // Change the fxml data with CompleteData TODO
-    public void setFxmlData(CompleteWeatherData completeWeatherData){
 
+    /* *** --------- ***
+    // *** Gui ***
+    // *** --------- ***/
+
+    // Change the fxml data with CompleteData
+    public void setFxmlData(CompleteWeatherData completeWeatherData){
+        // Current Day
+        changeCurrentGui(completeWeatherData.getCurrent());
+        changeDayGui(completeWeatherData.getForecastDayList().get(0),0);
     }
 
+    // Change Current
+    public void changeCurrentGui(Current current){
+        // Current Day
+        currentConditionLabelTemperature.setText(String.valueOf(current.getTemp_c())); // Current Temp
+        currentConditionLabelPressure.setText(String.valueOf(current.getPressure_mb())); // Current pressure
+        // currentConditionLabelRain.setText(String.valueOf(current.)); TODO uitzoeken,niet beschikbaar
+        currentConditionLabelWind.setText(String.valueOf(current.getWind_kph())); // Current Wind
+        currentConditionLabelState.setText(current.getConditionText()); // Current condition
+        // Symbol Change
+        Symbol symbol = new Symbol();
+        String url = symbol.getWeatherSymbolURL(current.getConditionCode());
+        try {
+            File file = new File(url);
+            Image image = new Image(file.toURI().toString());
+            currentConditionImageViewIcon.setImage(image);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    // Change Days TODO text naar een object(bv label),zo komt er een mooie loop
+    public void changeDayGui(ForecastDay forecastDay,int numberOfDay){
 
 
+    }
 
 
 }
